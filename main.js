d967ffd8c6951b4afef246b0be7438b27645c72a
Tony Nguyen <tonynguyen7396@gmail.com>
2024-07-11 11:50:48 +0200

    Added a toggle button to show hidden contact info

--- main.js
+++ main.js
@@ -20,3 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleContacts");
+  let contactsHidden = true;
+
+  toggleButton.addEventListener("click", function () {
+    const cardTextElements = document.querySelectorAll(".card-text");
+
+    cardTextElements.forEach(function (element) {
+      const extractedText = element.getAttribute("onclick");
+      const email = extractedText.match(/ .*m/); //IM ABOUT TO LOSE IT, THIS TOOK 2 HOURS TO FIGURE OUT
+
+      if (contactsHidden) {
+        element.textContent = `Contact: ${email}`;
+      } else {
+        element.textContent = `Contact: ***`;
+      }
+    });
+
+    contactsHidden = !contactsHidden;
+  });
+});
